e92d396605e73396bdfea637db87df33be80124f
Maxim Edelson <medel003@ucr.edu>
2020-05-19 09:23:58 -0700

    Lab 8

---
+++ Labs/Lab 8 Movie Sentiment Analysis/WordEntry.h
@@ -0,0 +1,24 @@
+#ifndef WORDENTRY_H
+#define WORDENTRY_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class WordEntry {
+
+ private:
+	string word;
+	int numAppearances;
+	int totalScore;
+ public:
+	WordEntry(const string &, int);
+	void addNewAppearance(int);
+	const string & getWord();
+	double getAverage();
+	double getScore() const;
+	int getAppearances() const;
+};
+#endif

d47e47015d0002758a62514184f4a4af9c4f5ac9
Maxim Edelson <medel003@ucr.edu>
2020-05-20 13:57:01 -0700

    bug fixes lab 8

--- Labs/Lab 8 Movie Sentiment Analysis/HashTable.cpp
+++ Labs/Lab 8 Movie Sentiment Analysis/HashTable.cpp
@@ -7,95 +7,9 @@
 #include <iterator>
 #include <fstream>
 #include <cstdlib>
+#include <math.h>
 
 using namespace std;
-
-int main() {
-        // declare a few needed variables for inputing the data
-        string line;
-        int score;
-        string message = " ";
-
-        // open input file
-        ifstream myfile("movieReviews.txt");
-        if (myfile.fail()) {
-            cout << "could not open file" << endl;
-            exit(1);
-        }
-         
-        //create hash table
-        HashTable table(20071);
-         
-        while (!myfile.eof()) {
-            myfile >> score;     // get score
-            myfile.get();        // get blank space
-            getline(myfile, line);
-            int len = line.size();
-            while(len > 0) {     // identify all individual strings
-                string sub; 
-                len = line.find(" ");
-                if (len > 0) { 
-                    sub = line.substr(0, len);
-                    line = line.substr(len + 1, line.size());
-                }
-                else {
-                    sub = line.substr(0, line.size() - 1);
-                }
-                table.put(sub, score); // insert string with the score
-            }
-        }
-          
-	// after data is entered in hash function
-        // prompt user for a new movie review
-        while(message.length() > 0) {
-            cout << "enter a review -- Press return to exit: " << endl;
-            getline(cin, message);
-            
-            // used for calculating the average
-            double sum = 0;
-            int count = 0;
-                
-            double sentiment = 0.0;
-    
-            size_t len = message.size();
-            // get each individual word from the input
-            while(len != string::npos) {
-                string sub;
-                len = message.find(" "); 
-                if (len != string::npos) {
-                    sub = message.substr(0, len);
-                    message = message.substr(len + 1, message.size());
-                }   
-                else {
-                    sub = message;
-                }   
-                // calculate the score of each word
-                sum += table.getAverage(sub);
-                ++count;
-            }   
-    
-            if (message.size() > 0) {
-                sentiment = sum / count;
-                cout << "The review has an average value of " << sentiment << endl;
-                if (sentiment >= 3.0) {
-                        cout << "Positive Sentiment" << endl;
-                }   
-                else if (sentiment >= 2.0) {
-                        cout << "Somewhat Positive Sentiment" << endl;
-                }   
-                else if (sentiment >= 1.0) {
-                        cout << "Somewhat Negative Sentiment" << endl;
-                }   
-                else {
-                        cout << "Negative Sentiment" << endl;
-                }   
-                cout << endl;
-            }   
-        }   
-    
-        return 0;
-}
-
 
 
 /* HashTable constructor
@@ -119,12 +33,9 @@ int HashTable::computeHash(const string &s) {
 	int hashVal = 0;
 
 	for (unsigned i = 0; i < s.size() && i < 3; i++) {
-		if (i == 0)
-			hashVal = s.at(i);
-		else if (i == 1)
-			hashVal += (s.at(i) * 32);
-		else
-			hashVal += (s.at(i) * (32 * 32));
+		for (unsigned j = 0; j < i; j++) {
+			hashVal += s.at(i) * pow(32, j);
+		}
 	}
 
 	return hashVal % size;

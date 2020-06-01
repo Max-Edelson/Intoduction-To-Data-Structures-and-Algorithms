32bc1b05730ab4c1f34f8ee666aca14809ce6ad6
Maxim Edelson <medel003@ucr.edu>
2020-06-01 16:33:53 -0700

    Bug Fixes

--- Labs/Lab 10 Graph/Lab 10 Graph/Graph.cpp
+++ Labs/Lab 10 Graph/Lab 10 Graph/Graph.cpp
@@ -26,6 +26,7 @@ Graph::Graph(ifstream& input) {
 			vertices.push_back(vertex);
 		}
 
+
 		string vertexA;
 		string vertexB;
 		int vertexAPos;
@@ -37,11 +38,11 @@ Graph::Graph(ifstream& input) {
 			input >> vertexB;
 			input >> cost;
 
-			for (int j = 0; j < vertices.size(); j++) {
+			for (unsigned j = 0; j < vertices.size(); j++) {
 				if (vertices.at(j).label == vertexA) {
 					vertexAPos = j;
 				}
-				else if (vertices.at(i).label == vertexB) {
+				else if (vertices.at(j).label == vertexB) {
 					vertexBPos = j;
 				}
 			}
@@ -49,7 +50,7 @@ Graph::Graph(ifstream& input) {
 			pair <int, int> neighbor;
 			neighbor.second = cost;
 
-			for (int k = 0; k < vertices.size(); k++) {
+			for (unsigned k = 0; k < vertices.size(); k++) {
 				if (vertices.at(k).label == vertexA) {
 					neighbor.first = vertexBPos;
 					vertices.at(k).neighbors.push_back(neighbor);
@@ -72,8 +73,6 @@ void Graph::bfs() {
 
 	queue<int>	queue;
 
-	Vertex& startNode = vertices.at(0);
-
 	queue.push(0);
 	vertices.at(0).distance = 0;
 
@@ -83,11 +82,11 @@ void Graph::bfs() {
 
 		int target;
 
-		list<pair<int,int>>::iterator itList;
+		list< pair<int,int> >::iterator itList;
 		for (itList = vertices.at(u).neighbors.begin(); itList != vertices.at(u).neighbors.end(); itList++) {
 			target = itList->first;
 			if (vertices.at(target).color == "WHITE") {
-				vertices.at(target).color == "GREY";
+				vertices.at(target).color = "GREY";
 				vertices.at(target).distance = vertices.at(u).distance + 1;
 				//vertices.at(target).prev->u;
 				vertices.at(target).prev = &vertices.at(u);
@@ -103,11 +102,24 @@ void Graph::printGraph() const {
 	for (unsigned i = 0; i < vertices.size(); i++) {
 		cout << "Name: " << vertices.at(i).label << " Color: " << vertices.at(i).color << " Distance: " << vertices.at(i).distance << endl;
 	}
+
+	cout << endl;
+
+	for (unsigned i = 0; i < vertices.size(); i++) {
+		cout << vertices.at(i).label << endl;
+	}
+	
+	for (unsigned i = 0; i < vertices.size(); i++) { 
+		list< pair<int,int> >::const_iterator itList;
+		for (itList = vertices.at(i).neighbors.begin(); itList != vertices.at(i).neighbors.end(); itList++) {
+			cout << vertices.at(i).label << " " << vertices.at(itList->first).label << " " << itList->second << endl;
+		}
+	}
 }
 
 void Graph::output_graph(const string& filename, const Vertex start) {
 	for	(unsigned i = 0; i < vertices.size(); i++) {
-		list<pair<int, int>>::iterator itList;
+		list< pair<int, int> >::iterator itList;
 	}
 }
 
